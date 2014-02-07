9bf41065a174b0c97ef66da0cb2a3a20a4410440
Andrew Cassidy <drewcassidy@me.com>
2014-02-07 10:55:35 -0800

    Re-added background star field and a custom editor layout.

---
+++ Assets/UnityScript/MonoBehaviour/Object Scripts/StarField.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+//var mainCam : GameObject;
+var mainTransform : Transform;
+//var selfTransform : Transform;
+
+function Start () {
+	yield WaitForSeconds(1);
+
+	var starSystems : Component[];
+	starSystems = GetComponentsInChildren(ParticleSystem);
+	for (var i : Component in starSystems){
+		i.particleSystem.Pause();
+	}
+
+}
+
+function Update () {
+	var Transform = GetComponent(Transform);
+	Transform.position = mainTransform.position;
+}
