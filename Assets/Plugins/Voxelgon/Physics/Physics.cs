91470e5f5c09637bb1e1078a0af89931ca61e62f
Andrew Cassidy <drewcassidy@me.com>
2014-02-09 20:17:16 -0800

    Modified elasticDeltaV to have change in velocity, and documentation

--- Assets/Plugins/Voxelgon/Physics/Physics.cs
+++ Assets/Plugins/Voxelgon/Physics/Physics.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 namespace Voxelgon {
 	public class Physics { 
+		//returns the change in velocity when two rigidbodys collide. For use
+		//with unityengine's OnCollisionEnter() message. 
 		public static float elasticDeltaV(Rigidbody r1,Rigidbody r2) {
 			float u1 = r1.velocity.magnitude;
 			float u2 = r2.velocity.magnitude;
@@ -10,7 +12,7 @@ namespace Voxelgon {
 			float m2 = r2.mass;
 
 			float v1 = (u1*(m1-m2)+(2*(m2*u2)))/(m1+m2);
-			return(v1);
+			return(mathf.abs(r1 - v1));
 		}
 	}
 }
