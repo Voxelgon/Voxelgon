9fae47b01ee96140d32e153e7127ee5fced17f78
Andrew Cassidy <drewcassidy@me.com>
2014-02-09 16:31:56 -0800

    Added ElasticDeltaV method, as well as physics and soft body classes

---
+++ Assets/Plugins/Voxelgon/Physics/Physics.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Voxelgon {
+	public class Physics { 
+		public static float elasticDeltaV(Rigidbody r1,Rigidbody r2) {
+			float u1 = r1.velocity.magnitude;
+			float u2 = r2.velocity.magnitude;
+			float m1 = r1.mass;
+			float m2 = r2.mass;
+
+			float v1 = (u1*(m1-m2)+(2*(m2*u2)))/(m1+m2);
+			return(v1);
+		}
+	}
+}
---
+++ Assets/Plugins/Voxelgon/Physics/Softbody.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Voxelgon {
+	public class Softbody : Physics {
+
+	}
+}
