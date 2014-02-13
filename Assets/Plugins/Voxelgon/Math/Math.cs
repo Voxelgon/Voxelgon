100341b1b166ab11509d8daa876b05fe1d869da3
Andrew Cassidy <drewcassidy@me.com>
2014-02-12 19:30:11 -0800

    make code more readable

--- Assets/Plugins/Voxelgon/Math/Math.cs
+++ Assets/Plugins/Voxelgon/Math/Math.cs
@@ -3,21 +3,24 @@ using System.Collections;
 
 namespace Voxelgon {
 	public class Math {	
+		
 		//returns the angle in degrees from the x axis to a line between origin and child
 		//2d (x,z) only right now
 		public static float TwoPointAngle(Vector3 origin, Vector3 child) {
 			Vector3 deltaT = child - origin;
-
 			float angle = Mathf.Atan(deltaT.z/deltaT.x) * Mathf.Rad2Deg;
 			angle = (angle + 360) % 360;
+			
 			return angle;
 		}
+		
 		//returns the difference in angle between two points relative to the origin in degrees.
 		//2d (x,z) only right now		
 		public static float ThreePointAngle(Vector3 origin, Vector3 child1, Vector3 child2) {
 			float angle1 = Math.TwoPointAngle(origin, child1);
 			float angle2 = Math.TwoPointAngle(origin, child2);
 			float angle = (angle2-angle1);
+			
 			return angle;
 		}
 	}
--- Assets/Plugins/Voxelgon/Physics/Physics.cs
+++ Assets/Plugins/Voxelgon/Physics/Physics.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 namespace Voxelgon {
 	public class Physics { 
+		
 		//returns the change in velocity when two rigidbodys collide. For use
 		//with unityengine's OnCollisionEnter() message. 
 		public static float ElasticDeltaV(Rigidbody r1,Rigidbody r2) {
@@ -12,6 +13,7 @@ namespace Voxelgon {
 			float m2 = r2.mass;
 
 			float v1 = (u1*(m1-m2)+(2*(m2*u2)))/(m1+m2);
+			
 			return(Mathf.Abs(u1 - v1));
 		}
 	}
