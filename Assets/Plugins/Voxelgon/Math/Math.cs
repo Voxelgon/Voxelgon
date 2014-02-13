7159bd1d3592d9975b4bca5a8bc4f8b5c6fd1d20
Andrew Cassidy <drewcassidy@me.com>
2014-02-12 17:49:13 -0800

    Created Voxelgon.Math class and the Math.TwoPointAngle() function

---
+++ Assets/Plugins/Voxelgon/Math/Math.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Voxelgon {
+	public class math {	
+		//returns the angle from the x axis to a line between origin and child
+		//2d (x,z) only right now
+		public static float TwoPointAngle(Vector3 origin, Vector3 child) {
+			Vector3 deltaT = child - origin;
+
+			float angle = Mathf.Atan(deltaT.z/deltaT.x) * Mathf.Rad2Deg;
+			angle = (angle + 360) % 360;
+			return angle;
+		}
+	}
+}
---
+++ Assets/PluginsJS/MonoBehaviour/Object Scripts/ShipControl.js
@@ -0,0 +1,2 @@
+﻿#pragma strict
+
