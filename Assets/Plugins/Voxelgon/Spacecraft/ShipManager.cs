06c0bbdcd3349c20c1ae26b613f872f57b7a04d5
Andrew Cassidy <drewcassidy@me.com>
2014-02-16 00:38:08 -0800

    recombined if/else statements, fixed rotation checking for angles close to +/-180

--- Assets/Plugins/Voxelgon/Spacecraft/ShipManager.cs
+++ Assets/Plugins/Voxelgon/Spacecraft/ShipManager.cs
@@ -52,38 +52,37 @@ public class ShipManager : MonoBehaviour {
 		foreach(Component port in PortScripts) {
 			float angle = Voxelgon.Math.RelativeAngle(origin, port.transform);
 			float childAngle = port.transform.localEulerAngles.y;
-			//Debug.Log(angle);
+			Debug.Log(angle);
 
-			if(angle > portYawCutoff){
+			if((angle > portYawCutoff) && (angle < 180 - portYawCutoff)){
 				yawPorts[YawState.YawLeft].Add(port.gameObject);
-				//Debug.Log("This port is for turning Left!");
+				Debug.Log("This port is for turning Left!");
 
-			} else if(angle < (-1 * portYawCutoff)){
+			} else if((angle < (-1 * portYawCutoff)) && (angle > (-1 * (180 - portYawCutoff)))){
 				yawPorts[YawState.YawRight].Add(port.gameObject);
-				//Debug.Log("This port is for turning right!");
+				Debug.Log("This port is for turning right!");
+
 			} else {
 				if((childAngle > 315 + portTransCutoff) || (childAngle < 45 - portTransCutoff)) {
 
 					//0 degrees
 					transPorts[TransState.TransForw].Add(port.gameObject);
-				}
-
-				if((childAngle > 45 + portTransCutoff) && (childAngle < 135 - portTransCutoff)) {
+					Debug.Log("This port is for translating forward!");
+				} else if((childAngle > 45 + portTransCutoff) && (childAngle < 135 - portTransCutoff)) {
 
 					//90 degrees
 					transPorts[TransState.TransRight].Add(port.gameObject);
-				}
-
-				if((childAngle > 135 + portTransCutoff) && (childAngle < 225 - portTransCutoff)) {
+					Debug.Log("This port is for translating right!");
+				} else if((childAngle > 135 + portTransCutoff) && (childAngle < 225 - portTransCutoff)) {
 
 					//180 degrees
 					transPorts[TransState.TransBack].Add(port.gameObject);
-				}
-
-				if((childAngle > 225 + portTransCutoff) && (childAngle < 315 - portTransCutoff)) {
+					Debug.Log("This port is for translating back!");
+				} else if((childAngle > 225 + portTransCutoff) && (childAngle < 315 - portTransCutoff)) {
 
 					//270 degrees
 					transPorts[TransState.TransLeft].Add(port.gameObject);
+					Debug.Log("This port is for translating left!");
 				}
 			}
 		}
