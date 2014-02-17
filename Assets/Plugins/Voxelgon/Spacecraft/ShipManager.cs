79c63d6c2c8d80b4aee20765b11048e9657f33e0
Andrew Cassidy <drewcassidy@me.com>
2014-02-16 17:51:31 -0800

    Correct Categorization
    + Recombined enums
    + input updates for ports to check

--- Assets/Plugins/Voxelgon/Spacecraft/ShipManager.cs
+++ Assets/Plugins/Voxelgon/Spacecraft/ShipManager.cs
@@ -17,33 +17,36 @@ public class ShipManager : MonoBehaviour {
 	public float yawAxis;
 
 	//Setup Variables for gathering Ports
-	public enum YawState {
+	public enum PortState {
 		YawLeft,
-		YawRight
-	}
-
-	public enum TransState {
+		YawRight,
 		TransLeft,
 		TransRight,
 		TransForw,
 		TransBack
 	}
 
+	//translation states for ports to access
+	public int yaw;
+	public int lin;
+	public int lat;
+
 	//dictionaries of ports
-	public Dictionary<YawState, List<GameObject> > yawPorts = new Dictionary<YawState, List<GameObject> > ();
-	public Dictionary<TransState, List<GameObject> > transPorts = new Dictionary<TransState, List<GameObject> > ();
+	public Dictionary<PortState, List<GameObject> > ports = new Dictionary<PortState, List<GameObject> > ();
 
 	public void SetupPorts(){
 		
 		//Yaw port lists
-		yawPorts.Add(YawState.YawLeft, new List<GameObject>());
-		yawPorts.Add(YawState.YawRight,new List<GameObject>());
+		ports.Add(PortState.YawLeft, new List<GameObject>());
+		ports.Add(PortState.YawRight,new List<GameObject>());
 		
-		//Translation port lists
-		transPorts.Add(TransState.TransLeft, new List<GameObject>());
-		transPorts.Add(TransState.TransRight,new List<GameObject>());
-		transPorts.Add(TransState.TransForw, new List<GameObject>());
-		transPorts.Add(TransState.TransBack, new List<GameObject>());
+		//Linear port lists
+		ports.Add(PortState.TransForw, new List<GameObject>());
+		ports.Add(PortState.TransBack, new List<GameObject>());
+		
+		//Lateral ports lists
+		ports.Add(PortState.TransLeft, new List<GameObject>());
+		ports.Add(PortState.TransRight,new List<GameObject>());
 
 		Vector3 origin = transform.rigidbody.centerOfMass;
 		//Debug.Log(origin);
@@ -56,33 +59,41 @@ public class ShipManager : MonoBehaviour {
 			Debug.Log(angle);
 
 			if((angle > portYawCutoff) && (angle < 180 - portYawCutoff)){
-				yawPorts[YawState.YawLeft].Add(port.gameObject);
+				
+				//30 degrees to 150 degrees
+				ports[PortState.YawLeft].Add(port.gameObject);
 				Debug.Log("This port is for turning Left!");
 
 			} else if((angle < (-1 * portYawCutoff)) && (angle > (-1 * (180 - portYawCutoff)))){
-				yawPorts[YawState.YawRight].Add(port.gameObject);
+				
+				//-150 degrees to -30 degrees
+				ports[PortState.YawRight].Add(port.gameObject);
 				Debug.Log("This port is for turning right!");
 
 			} else {
+
 				if((childAngle > 315 + portTransCutoff) || (childAngle < 45 - portTransCutoff)) {
 
 					//0 degrees
-					transPorts[TransState.TransForw].Add(port.gameObject);
+					ports[PortState.TransForw].Add(port.gameObject);
 					Debug.Log("This port is for translating forward!");
+
 				} else if((childAngle > 45 + portTransCutoff) && (childAngle < 135 - portTransCutoff)) {
 
 					//90 degrees
-					transPorts[TransState.TransRight].Add(port.gameObject);
+					ports[PortState.TransRight].Add(port.gameObject);
 					Debug.Log("This port is for translating right!");
+
 				} else if((childAngle > 135 + portTransCutoff) && (childAngle < 225 - portTransCutoff)) {
 
 					//180 degrees
-					transPorts[TransState.TransBack].Add(port.gameObject);
+					ports[PortState.TransBack].Add(port.gameObject);
 					Debug.Log("This port is for translating back!");
+
 				} else if((childAngle > 225 + portTransCutoff) && (childAngle < 315 - portTransCutoff)) {
 
 					//270 degrees
-					transPorts[TransState.TransLeft].Add(port.gameObject);
+					ports[PortState.TransLeft].Add(port.gameObject);
 					Debug.Log("This port is for translating left!");
 				}
 			}
@@ -91,9 +102,13 @@ public class ShipManager : MonoBehaviour {
 	
 	//updates input variables
 	public void UpdateInputs() {
+		yawAxis = Input.GetAxis("Yaw");	
 		linAxis = Input.GetAxis("Thrust");
 		latAxis = Input.GetAxis("Strafe");
-		yawAxis = Input.GetAxis("Yaw");		
+
+		yaw = (int) yawAxis;
+		lin = (int) linAxis;
+		lat = (int) latAxis;
 	}
 
 	//Called every frame
