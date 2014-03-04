b950a6959a60f3f11a78e7f63fd15fdb2b0ff314
Andrew Cassidy <drewcassidy@me.com>
2014-03-03 19:38:10 -0800

    Add base classes for Asset management

---
+++ Assets/Plugins/Voxelgon/Asset.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+namespace Voxelgon{
+
+  static class Asset{
+
+    static public void Import() {
+
+    }
+  }
+}
---
+++ Assets/Plugins/Voxelgon/Manager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Voxelgon {
+  public class Manager : MonoBehaviour {
+
+    public void start() {
+
+    }
+  }
+}
