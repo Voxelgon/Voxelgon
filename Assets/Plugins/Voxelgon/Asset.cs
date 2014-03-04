71c316e6da4a6aac1f5e32fea21f4c1b1c12cd93
Andrew Cassidy <drewcassidy@me.com>
2014-03-03 21:46:38 -0800

    comment stuff up nicely

--- Assets/Plugins/Voxelgon/Asset.cs
+++ Assets/Plugins/Voxelgon/Asset.cs
@@ -12,21 +12,26 @@ namespace Voxelgon{
 
         static string resourcePath;
 
+
+        //returns the parent (../) directory of the given path
         static public string Parent(string path) {
             string parent = Directory.GetParent(path).FullName;
             return parent;
         }
 
+        //returns a list of files under the given path directory
         static public List<string> GetFiles(string path) {
             List<string> files = new List<string>(Directory.GetFiles(path));
             return files;
         }
 
+        //returns a list of directories under the given directory
         static public List<string> GetDirectories(string path) {
             List<string> files = new List<string>(Directory.GetDirectories(path));
             return files;
         }
 
+        //returns a list of all files under the given directory in the file tree
         static public List<string> FilesUnderDirectory(string path) {
             List<string> directories = GetDirectories(path);
 
@@ -39,6 +44,8 @@ namespace Voxelgon{
             return files;
         }
 
+
+        //imports assets (all testing code right now)
         static public void Import() {
             resourcePath = Parent(Application.dataPath) + "/Resources";
             Debug.Log(resourcePath);
