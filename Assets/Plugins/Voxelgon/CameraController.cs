fe67439e7c2bb32baa5fab1010ef30db8308e097
Andrew Cassidy <drewcassidy@me.com>
2015-05-17 12:46:01 -0700

    Add zoom and sensitivity
    + add zoom control with adjustable exponent
    + add sensitivity variables
    - remove debug messages

--- Assets/Plugins/Voxelgon/CameraController.cs
+++ Assets/Plugins/Voxelgon/CameraController.cs
@@ -3,10 +3,19 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
+	public float panSensitivity = 0.8f;
+	public float orbitSensitivity = 3f;
+	public float zoomSensativity = 1f;
+	public float zoomExpo = 1.1f;
+
 	public float minAltitude = 5;
 	public float maxAltitude = 85;
 
-	private float altitude = (45);
+	public float minZoom = 1;
+	public float maxZoom = 1000;
+
+	private float zoom = 10;
+	private float altitude = 45;
 
 	private Quaternion altitudeQuat = new Quaternion();
 
@@ -19,13 +28,14 @@ public class CameraController : MonoBehaviour {
 		orthoSpace = Matrix4x4.TRS(Vector3.one, Quaternion.Euler(-altitude,0,0), Vector3.one);
 
 		if (Input.GetAxis("Pan") == 1) {
-			transform.parent.Translate(orthoSpace.MultiplyVector(new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"))));
+			transform.parent.Translate(orthoSpace.MultiplyVector(panSensitivity * new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"))));
 		}
 		if (Input.GetAxis("Orbit") == 1) {
-			transform.parent.Rotate(Vector3.up * Input.GetAxis("Mouse X"),Space.World);
-			transform.parent.Rotate(Vector3.right * (Mathf.Clamp(Input.GetAxis("Mouse Y"), minAltitude - altitude, maxAltitude - altitude)),Space.Self);
-			Debug.Log(minAltitude-altitude);
-
+			transform.parent.Rotate(Vector3.up * orbitSensitivity * Input.GetAxis("Mouse X"),Space.World);
+			transform.parent.Rotate(Vector3.right * (Mathf.Clamp(orbitSensitivity * Input.GetAxis("Mouse Y"), minAltitude - altitude, maxAltitude - altitude)),Space.Self);
 		}
+
+		zoom = -transform.localPosition.z ;
+		transform.Translate( Vector3.back * Mathf.Clamp(Input.GetAxis("zoom") * zoomSensativity * Mathf.Pow(zoom, zoomExpo), minZoom - zoom, maxZoom - zoom));
 	}
 }
