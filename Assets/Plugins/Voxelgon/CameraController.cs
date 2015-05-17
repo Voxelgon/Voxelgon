5bab8c4809e2afebc5db5d8d5dc7e94a569be5c3
Andrew Cassidy <drewcassidy@me.com>
2015-05-17 13:56:46 -0700

    clean up camera controller script
    + add documentation
    • clean up camera controller script

--- Assets/Plugins/Voxelgon/CameraController.cs
+++ Assets/Plugins/Voxelgon/CameraController.cs
@@ -23,19 +23,24 @@ public class CameraController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		altitude = transform.parent.eulerAngles.x ;
 
+		//set values of temporary variables
+		altitude = transform.parent.eulerAngles.x ;
+		zoom = -transform.localPosition.z ;
 		orthoSpace = Matrix4x4.TRS(Vector3.one, Quaternion.Euler(-altitude,0,0), Vector3.one);
 
+		//pan camera (default to RMB)
 		if (Input.GetAxis("Pan") == 1) {
 			transform.parent.Translate(orthoSpace.MultiplyVector(panSensitivity * new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"))));
 		}
+
+		//orbit camera (default to MMB)
 		if (Input.GetAxis("Orbit") == 1) {
 			transform.parent.Rotate(Vector3.up * orbitSensitivity * Input.GetAxis("Mouse X"),Space.World);
 			transform.parent.Rotate(Vector3.right * (Mathf.Clamp(orbitSensitivity * Input.GetAxis("Mouse Y"), minAltitude - altitude, maxAltitude - altitude)));
 		}
 
-		zoom = -transform.localPosition.z ;
+		//zoom camera (default to Scroll Wheel)
 		transform.Translate( Vector3.back * Mathf.Clamp(Input.GetAxis("zoom") * zoomSensativity * Mathf.Pow(zoom, zoomExpo), minZoom - zoom, maxZoom - zoom));
 	}
 }
