9d1e05cb0622cd9ee9ad8534aa3c40ff18245f4d
Andrew Cassidy <drewcassidy@me.com>
2015-10-03 11:47:47 -0700

    Add cursor for editing
    + add code to find where the cursor overlaps the y=0 plane
    + add editing cursor

--- Assets/Plugins/Voxelgon/CameraController.cs
+++ Assets/Plugins/Voxelgon/CameraController.cs
@@ -17,7 +17,18 @@ public class CameraController : MonoBehaviour {
 	private float zoom = 10;
 	private float altitude = 45;
 
+	private Collider floorCollider;
+
 	private Matrix4x4 orthoSpace = new Matrix4x4();
+
+	public Mode currentMode;
+
+	public enum Mode {edit, flight}
+
+	public GameObject editorCursor;
+
+	void Start () {
+	}
 
 	// Update is called once per frame
 	void Update () {
@@ -43,6 +54,20 @@ public class CameraController : MonoBehaviour {
 		//zoom camera (default to Scroll Wheel)
 		transform.Translate( Vector3.back * Mathf.Clamp(Input.GetAxis("zoom") * zoomSensativity * Mathf.Pow(zoom, zoomExpo), minZoom - zoom, maxZoom - zoom));
 
-		////Click Handling////
+
+		////Cursor Handling////
+		if(currentMode == Mode.edit) {
+			Ray cursorRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			float xySlope = cursorRay.direction.y / cursorRay.direction.x;
+			float zySlope = cursorRay.direction.y / cursorRay.direction.z;
+
+			float xIntercept = cursorRay.origin.x + cursorRay.origin.y / -xySlope;
+			float zIntercept = cursorRay.origin.z + cursorRay.origin.y / -zySlope;
+
+			Vector3 InteceptPoint = new Vector3(xIntercept, 0, zIntercept);
+			Debug.DrawLine(cursorRay.origin, InteceptPoint, Color.yellow);
+			editorCursor.transform.position = InteceptPoint;
+		}
 	}
 }
