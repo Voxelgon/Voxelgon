39b6803e92231c3e47057dd32d27c0a18e00602b
Andrew Cassidy <drewcassidy@me.com>
2015-10-20 22:25:29 -0700

    HELP
    WHAT AM I DOING

--- Assets/Plugins/Voxelgon/GameMode.cs
+++ Assets/Plugins/Voxelgon/GameMode.cs
@@ -2,14 +2,9 @@
 using System.Collections;
 
 namespace Voxelgon {
-	public static class GameMode{
-
-		public static Gamemode current;
-
-		public enum Gamemode {
-			menu,
-			edit,
-			flight
-		}
+	public enum GameMode {
+		menu,
+		edit,
+		flight
 	}
 }
---
+++ Assets/Plugins/Voxelgon/test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class test : MonoBehaviour, IPointerEnterHandler {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnPointerEnter(PointerEventData data){
+		Debug.Log("hello, world!");
+	}
+}
