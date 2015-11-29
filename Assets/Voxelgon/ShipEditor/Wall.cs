440e82458a4decd04828a9e3d5691f3ef90a8270
Andrew Cassidy <drewcassidy@me.com>
2015-11-28 17:49:29 -0800

    Generate a world to plane matrix for walls

--- Assets/Voxelgon/ShipEditor/Wall.cs
+++ Assets/Voxelgon/ShipEditor/Wall.cs
@@ -16,6 +16,7 @@ namespace Voxelgon.ShipEditor {
 		private readonly Mesh complexMesh = new Mesh();
 
 		private Plane wallPlane;
+		private Matrix4x4 worldToPlaneMatrix;
 
 		private bool verticesChanged;
 
@@ -23,11 +24,13 @@ namespace Voxelgon.ShipEditor {
 
 		public Wall() {
 			wallPlane = new Plane();
+			worldToPlaneMatrix = new Matrix4x4();
 			Editor = GameObject.Find("ShipEditor").GetComponent<ShipEditor>();
 		}
 
 		public Wall(ShipEditor editor){
 			wallPlane = new Plane();
+			worldToPlaneMatrix = new Matrix4x4();
 			Editor = editor;
 		}
 
@@ -56,6 +59,9 @@ namespace Voxelgon.ShipEditor {
 		
 		public Mesh ComplexMesh {
 			get {
+				if (verticesChanged) {
+					BuildComplexMesh();
+				}
 				return complexMesh;
 			}
 		}
@@ -94,14 +100,26 @@ namespace Voxelgon.ShipEditor {
 
 		//Private Methods
 
+		private void BuildMatrix() {
+			wallPlane = new Plane(vertices[0], vertices[1], vertices[2]);
+
+			worldToPlaneMatrix = new Matrix4x4();
+
+			var offset = -1 * vertices[0];
+			var rotation = Quaternion.FromToRotation(wallPlane.normal, Vector3.up);
+			var scale = Vector3.one;
+
+			worldToPlaneMatrix.SetTRS(offset, rotation, scale);
+		}
+
 		private bool AddVertex(Vector3 vertex) {
 			if (!ValidVertex(vertex)) return false;
 
 			vertices.Add(vertex);
 			verticesChanged = true;
 
-			if (IsPolygon) {
-				wallPlane = new Plane(vertices[0], vertices[1], vertices[2]);
+			if (vertices.Count == 3) {
+				BuildMatrix();
 			}
 			
 			return true;
@@ -139,6 +157,10 @@ namespace Voxelgon.ShipEditor {
 
 				verticesChanged = false;
 			}
+		}
+
+		private void BuildComplexMesh() {
+
 		}
 	}
 }
