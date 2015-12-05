2688b8263055d3ecbaeb829ba3f003aefbd8e8e5
WilliamBerryiii <wberry@lucidmotions.net>
2015-12-05 10:33:05 -0800

    Implemented proper equals for Position struct Minor code clean up Began updating naming of properties and fields

--- Assets/Voxelgon/Math/Position.cs
+++ Assets/Voxelgon/Math/Position.cs
@@ -1,43 +1,67 @@
 using UnityEngine;
 
 namespace Voxelgon.Math {
-	public struct Position {
-		short x;
-		short y;
-		short z;
+    public struct Position {
+        
+        public bool Equals(Position other)
+        {
+            return _x == other._x && _y == other._y && _z == other._z;
+        }
 
-		public Position(short x, short y, short z) {
-			this.x = x;
-			this.y = y;
-			this.z = z;
-		}
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is Position && Equals((Position) obj);
+        }
 
-		public Position(float x, float y, float z) {
-			this.x = (short) x;
-			this.y = (short) y;
-			this.z = (short) z;
-		}
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = _x.GetHashCode();
+                hashCode = (hashCode*397) ^ _y.GetHashCode();
+                hashCode = (hashCode*397) ^ _z.GetHashCode();
+                return hashCode;
+            }
+        }
 
-		public Position(Vector3 v) {
-			this.x = (short) v.x;
-			this.y = (short) v.y;
-			this.z = (short) v.z;
-		}
+        readonly short _x;
+        readonly short _y;
+        readonly short _z;
 
-		public static explicit operator Position(Vector3 v) {
-			return new Position(v);
-		}
+        public Position(short x, short y, short z) {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
 
-		public static explicit operator Vector3(Position pos) {
-			return new Vector3(pos.x, pos.y, pos.z);
-		}
+        //ToDo: Consider what float equality means here
+        public Position(float x, float y, float z) {
+            _x = (short) x;
+            _y = (short) y;
+            _z = (short) z;
+        }
 
-		public static bool operator ==(Position a, Position b) {
-			return (a.x == b.x && a.y == b.y && a.z == b.z);
-		}
+        public Position(Vector3 v) {
+            _x = (short) v.x;
+            _y = (short) v.y;
+            _z = (short) v.z;
+        }
 
-		public static bool operator !=(Position a, Position b) {
-			return (a.x != b.x || a.y != b.y || a.z != b.z);
-		}
-	}
+        public static explicit operator Position(Vector3 v) {
+            return new Position(v);
+        }
+
+        public static explicit operator Vector3(Position pos) {
+            return new Vector3(pos._x, pos._y, pos._z);
+        }
+
+        public static bool operator ==(Position a, Position b) {
+            return (a._x == b._x && a._y == b._y && a._z == b._z);
+        }
+
+        public static bool operator !=(Position a, Position b) {
+            return (a._x != b._x || a._y != b._y || a._z != b._z);
+        }
+    }
 }
--- Assets/Voxelgon/Ship/ShipManager.cs
+++ Assets/Voxelgon/Ship/ShipManager.cs
@@ -1,69 +1,77 @@
-﻿using UnityEngine;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Voxelgon;
+﻿using System;
+using UnityEngine;
 
-[RequireComponent (typeof (Rigidbody))]
+namespace Assets.Voxelgon.Ship
+{
+    [RequireComponent (typeof (Rigidbody))]
 
-public class ShipManager : MonoBehaviour {
+    public class ShipManager : MonoBehaviour {
+        private const double FloatCompareTolerance = 0.0000001;
 
-    public float portYawCutoff = 30;    //angle +/- before the port is no longer for rotation
+        //angle +/- before the port is no longer for rotation
+        public float PortYawCutoff = 30;    
 
-    //input Variables
-    public float linInput;
-    public float latInput;
-    public float yawInput;
+        //input Variables
+        public float LinInput;
+        public float LatInput;
+        public float YawInput;
 
-    public bool killRot;
-    public bool killTrans;
+        public bool KillRot;
+        public bool KillTrans;
 
-    //translation/yaw
-    public float yaw;
-    public float lin;
-    public float lat;
+        //translation/Yaw
+        public float Yaw;
+        public float Lin;
+        public float Lat;
 
-    public float brakingYaw = 0;
-    public float brakingLin = 0;
-    public float brakingLat = 0;
+        public float BrakingYaw;
+        public float BrakingLin;
+        public float BrakingLat;
 
-    private void FixedUpdate() {
-        yawInput = Input.GetAxis("Yaw");
-        linInput = Input.GetAxis("Thrust");
-        latInput = Input.GetAxis("Strafe");
+        private void FixedUpdate() {
+            YawInput = Input.GetAxis("Yaw");
+            LinInput = Input.GetAxis("Thrust");
+            LatInput = Input.GetAxis("Strafe");
 
-        if(Input.GetButtonUp("Kill Rotation")) {
-            killRot = !killRot;
+            if(Input.GetButtonUp("Kill Rotation")) {
+                KillRot = !KillRot;
+            }
+
+            if(Input.GetButtonUp("Kill Translation")) {
+                KillTrans = !KillTrans;
+            }
+
+            //TODO: PID controller for this
+            if((KillRot) && (Math.Abs(YawInput) < FloatCompareTolerance) && (Mathf.Abs(GetComponent<Rigidbody>().angularVelocity.y) > 0.01)) {
+                BrakingYaw = -GetComponent<Rigidbody>().angularVelocity.y;
+
+            } else {
+                BrakingYaw = 0;
+            }
+
+            if(KillTrans 
+                && (Math.Abs(LinInput) < FloatCompareTolerance) 
+                && (Math.Abs(LatInput) < FloatCompareTolerance) 
+                && (Mathf.Abs(transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity).x) > 0.1)) {
+                BrakingLin = (int) -Mathf.Sign(transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity).x);
+
+            } else {
+                BrakingLin = 0;
+            }
+
+            if(KillTrans 
+                && (Math.Abs(LatInput) < FloatCompareTolerance)
+                && (Math.Abs(LinInput) < FloatCompareTolerance) 
+                && (Mathf.Abs(transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity).z) > 0.1)) {
+                BrakingLat = (int) Mathf.Sign(transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity).z);
+
+            } else {
+                BrakingLat = 0;
+            }
+
+            Yaw = YawInput + BrakingYaw;
+            Lin = LinInput + BrakingLin;
+            Lat = LatInput + BrakingLat;
         }
-
-        if(Input.GetButtonUp("Kill Translation")) {
-            killTrans = !killTrans;
-        }
-
-        //TODO: PID controller for this
-        if((killRot) && (yawInput == 0) && (Mathf.Abs(GetComponent<Rigidbody>().angularVelocity.y) > 0.01)) {
-            brakingYaw = -GetComponent<Rigidbody>().angularVelocity.y;
-
-        } else {
-            brakingYaw = 0;
-        }
-
-        if((killTrans) && (linInput == 0) && (latInput == 0) && (Mathf.Abs(transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity).x) > 0.1)) {
-            brakingLin = (int) -Mathf.Sign(transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity).x);
-
-        } else {
-            brakingLin = 0;
-        }
-
-        if((killTrans) && (latInput == 0)&& (linInput == 0) && (Mathf.Abs(transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity).z) > 0.1)) {
-            brakingLat = (int) Mathf.Sign(transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity).z);
-
-        } else {
-            brakingLat = 0;
-        }
-
-        yaw = yawInput + brakingYaw;
-        lin = linInput + brakingLin;
-        lat = latInput + brakingLat;
     }
 }
