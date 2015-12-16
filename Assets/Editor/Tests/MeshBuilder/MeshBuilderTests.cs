256905fac6fd6d15bff15dac7752cdc7faa826b4
Andrew Cassidy <drewcassidy@me.com>
2015-12-15 16:38:32 -0800

    split up unit tests for more granular feedback
    + add setup method to MeshBuilderTests
    + move all points testing with to a List
    • split up test methods for each subject being tested

--- Assets/Editor/Tests/MeshBuilder/MeshBuilderTests.cs
+++ Assets/Editor/Tests/MeshBuilder/MeshBuilderTests.cs
@@ -5,103 +5,117 @@ using NUnit.Framework;
 
 namespace Voxelgon.MeshBuilder.Tests {
     public class MeshBuilderTests {
+
+        private List<Vector3> points = new List<Vector3>
+        {
+            new Vector3(4, 0, 4),
+            new Vector3(2, 0, 0),
+            new Vector3(0, 0, 2),
+            new Vector3(2, 0, 2),
+            new Vector3(2, 0, 3),
+            new Vector3(2, 0, 4)   
+        };
+
+        private Vector3 normal = Vector3.up;
+        private MeshBuilder builder = new MeshBuilder("test", false);
+
+        //SETUP
+
+        [SetUp]
+        public void SetUp() {
+            builder.AddVertices(points, normal);
+        }
+
+        //TESTS
+
+        //WindingOrder
+
         [Test]
-        public void WindingOrder() {
-            //clockwise triangle
-            var point1 = new Vector3(1, 0, 3);
-            var point2 = new Vector3(7, 1, 0);
-            var point3 = new Vector3(0, 1, 3);
-
-            //colinear points
-            var point4 = new Vector3(1, 0, 2);
-            var point5 = new Vector3(1, 0, 4);
-            var point6 = new Vector3(1, 0, 3);
-
-            //Assert
+        public void WindingOrder_IsColinear0Deg() {
             Assert.That(
                 MeshBuilder.WindingOrder(
-                    point1,
-                    point2,
-                    point3,
+                    points[0],
+                    points[2],
+                    points[4],
+                    Vector3.up),
+                Is.EqualTo(0)
+            );
+        }
+
+        [Test]
+        public void WindingOrder_IsColinear180Deg() {
+            Assert.That(
+                MeshBuilder.WindingOrder(
+                    points[4],
+                    points[2],
+                    points[0],
+                    Vector3.up),
+                Is.EqualTo(0)
+            );
+        }
+
+        [Test]
+        public void WindingOrder_NormalDown_IsClockwise() {
+            Assert.That(
+                MeshBuilder.WindingOrder(
+                    points[0],
+                    points[1],
+                    points[2],
                     Vector3.up),
                 Is.EqualTo(1)
             );
 
             Assert.That(
                 MeshBuilder.WindingOrder(
-                    point3,
-                    point2,
-                    point1,
+                    points[2],
+                    points[1],
+                    points[0],
+                    Vector3.down),
+                Is.EqualTo(1)
+            );
+        }
+
+        [Test]
+        public void WindingOrder_IsCounterClockwise() {
+            Assert.That(
+                MeshBuilder.WindingOrder(
+                    points[2],
+                    points[1],
+                    points[0],
                     Vector3.up),
                 Is.EqualTo(-1)
             );
 
             Assert.That(
                 MeshBuilder.WindingOrder(
-                    point2,
-                    point1,
-                    point3,
+                    points[0],
+                    points[1],
+                    points[2],
                     Vector3.down),
-                Is.EqualTo(1)
-            );
-
-            Assert.That(
-                MeshBuilder.WindingOrder(
-                    point3,
-                    point2,
-                    point1,
-                    Vector3.down),
-                Is.EqualTo(1)
-            );
-
-            Assert.That(
-                MeshBuilder.WindingOrder(
-                    point4,
-                    point5,
-                    point6,
-                    Vector3.down),
-                Is.EqualTo(0)
-            );
-
-            Assert.That(
-                MeshBuilder.WindingOrder(
-                    point6,
-                    point5,
-                    point4,
-                    Vector3.down),
-                Is.EqualTo(0)
+                Is.EqualTo(-1)
             );
         }
 
+        //TriangleContains
+
         [Test]
-        public void TriangleContains() {
-            var builder = new MeshBuilder("test", false);
-            var points = new List<Vector3>();
-            points.Add(new Vector3(4, 0, 4));
-            points.Add(new Vector3(2, 0, 0));
-            points.Add(new Vector3(0, 0, 2));
-            points.Add(new Vector3(2, 0, 2));
-            points.Add(new Vector3(2, 0, 3));
-            points.Add(new Vector3(2, 0, 4));
-            var normal = new Vector3(0, 1, 0);
-
-            builder.AddVertices(points, normal);
-            
-            Assert.That(
-                builder.TriangleWindingOrder(0, 1, 2, normal),
-                Is.EqualTo(1)
-            );
-
+        public void TriangleContains_Inside() {
             Assert.That(
                 builder.TriangleContains(0, 1, 2, 3, normal),
                 Is.True
             );
+        }
 
+        [Test]
+        public void TriangleContains_OnEdge() {
             Assert.That(
                 builder.TriangleContains(0, 1, 2, 4, normal),
                 Is.True
             );
+        }
 
+        [Test]
+        public void TriangleContains_Outside() {
             Assert.That(
                 builder.TriangleContains(0, 1, 2, 5, normal),
                 Is.False
